c52f2d82bbe377f3f7b090f801c57eb9591d327b
hrishikesh <hshinde166@gmail.com>
2022-01-14 23:04:11 +0530

    Routing is done here

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import Home from './Pages/Home';
+import Cart from './Pages/Cart';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from './Pages/Signin';
+
+//firebase
+import firebaseConfig from './config/firebaseConfig';
+import { initializeApp } from "firebase/app";
+
+//Init firebase
+const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+    <Header />
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+      
+    </Router>
+    
+    </>
   );
 }
 
