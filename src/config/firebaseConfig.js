e6a78c64388b5ce5b6b70b69f60c6dc698eaed48
hrishikesh <hshinde166@gmail.com>
2022-01-14 23:08:20 +0530

    Firebase config file for the authentication

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB-MEQbnkhiiEoTVxiNu1SNaenPlRg4sWk",
+    authDomain: "clone-cb429.firebaseapp.com",
+    projectId: "clone-cb429",
+    storageBucket: "clone-cb429.appspot.com",
+    messagingSenderId: "1021002297057",
+    appId: "1:1021002297057:web:6e871cb2ffdbe9dd8491a0",
+    measurementId: "G-VWTQ7W5L44"
+  };
+
+export default firebaseConfig;
